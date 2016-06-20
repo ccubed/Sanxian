c9cb05435e9799a7de3f3308fe428f7e3312db2c
Cooper Click <ccubed.techno@gmail.com>
2016-06-19 22:13:02 -0400

    pagination works correctly

--- Sanxian.py
+++ Sanxian.py
@@ -6,6 +6,7 @@ import discord
 import ujson
 import redis
 from datetime import timedelta
+from key import *
 
 
 class Sanxian(discord.Client):
@@ -14,7 +15,7 @@ class Sanxian(discord.Client):
         self.voice = {}
         self.queues = {}
         self.prefixes = {}
-        self.commands = []
+        self.commands = [['play', self.enqueue], ['queue', self.show_queue]]
         rcon = redis.StrictRedis(db='5', encoding='utf-8')
         test = rcon.exists('Prefixes')
         if test:
@@ -24,31 +25,35 @@ class Sanxian(discord.Client):
     async def on_message(self, message):
         if message.author.bot or message.author == self.user:
             return
-        elif message.content.lower() == 'shinaide' and message.author.id == '66257033204080640':
+        elif message.content.lower() == 'siwang' and message.author.id == '66257033204080640':
             rcon = redis.StrictRedis(db='5', encoding='utf-8')
             rcon.set('Prefixes', ujson.dumps(self.prefixes))
             await self.logout()
-        elif message.startswith('saba'):
-            if message.content.split()[1].startswith('prefix') and message.channel.permissions_for(message.author).manage_server:
+        elif message.content.lower().startswith('saba'):
+            if message.content.split()[1].lower().startswith('prefix') and message.channel.permissions_for(message.author).manage_server:
                 self.prefixes[message.server.id] = message.content.split(':')[1]
-                await self.send_message(message.channel, "[Sanxian] Changed server prefix to {}".format(message.content.split(':')[1]))
+                await self.send_message(message.channel, "Changed server prefix to {}".format(message.content.split(':')[1]))
         else:
-            prefix = 'ongaku' if message.server.id not in self.prefixes else self.prefixes[message.server.id]
+            prefix = 'yinyue ' if message.server.id not in self.prefixes else self.prefixes[message.server.id]
             for command in self.commands:
                 if message.content.lower().startswith(prefix + command[0]):
                     ret = await command[1](message)
                     if isinstance(ret, str):
                         await self.send_message(message.channel, ret)
                     elif isinstance(ret, list):
-                        for x in ret:
-                            await self.send_message(message.channel, ret)
+                        attempt = "\n".join(ret)
+                        if len(attempt) > 2000:
+                            for x in ret:
+                                await self.send_message(message.channel, x)
+                        else:
+                            await self.send_message(message.channel, attempt)
 
     async def enqueue(self, message):
         url = message.content.split()[2]
         ytdl = await asyncio.create_subprocess_exec('youtube-dl', '-q', '-s', '--skip-download', '-J', url, stdout=asyncio.subprocess.PIPE)
-        await self.send_message(message.channel, "[Sanxian] Downloading information on {}".format(url))
+        await self.send_message(message.channel, "Downloading information on <{}>".format(url))
         stdout, _ = await ytdl.communicate()
-        if ytdl.retcode != 0:
+        if ytdl.returncode != 0:
             return "That video was not found or could not be downloaded by youtube-dl."
         jsd = ujson.loads(stdout.decode())
         if message.server.id not in self.queues:
@@ -56,32 +61,36 @@ class Sanxian(discord.Client):
         if 'entries' in jsd:  #Playlist
             for x in jsd['entries']:
                 self.queues[message.server.id].append([x['title'], x['webpage_url'], x['duration']])
-            return '[Sanxian] {} linked to a playlist called {} with {} tracks and a playtime of {}.'.format(url, jsd['title'],
-                                                                                                             len(jsd['entires']),
+            return '<{}> linked to a playlist called {} with {} tracks and a playtime of {}.'.format(url, jsd['title'],
+                                                                                                             len(jsd['entries']),
                                                                                                              timedelta(seconds=sum([x['duration'] for x in jsd['entries']])))
         else:  #Single video
             self.queues[message.server.id].append([jsd['title'], jsd['webpage_url'], jsd['duration']])
-            return '[Sanxian] {} linked to a video called {} with a playtime of {}.'.format(url, jsd['title'],
+            return '<{}> linked to a video called {} with a playtime of {}.'.format(url, jsd['title'],
                                                                                             timedelta(seconds=jsd['duration']))
 
     async def show_queue(self, message):
         if message.server.id not in self.queues or not len(self.queues[message.server.id]):
-            return "[Sanxian] Your server doesn't have a queue."
+            return "Your server doesn't have a queue."
         messages = []
         if ':' in message.content:  #pagination
             try:
                 page = int(message.content.split(":")[1])
             except:
-                return "[Sanxian] Page needs to be a number."
+                return "Page needs to be a number."
             for idx, x in enumerate(self.queues[message.server.id][(10*(page-1)):(10*page)]):
-                messages.append("{}. {} [{}]".format(idx, x[0], x[2]))
-            messages.append("{} - {} of {}  -  Page {} of {}  -  Use queue # for pages".format(10*(page-1), 10*page,
-                                                                                  len(self.queues[message.server.id]), page,
-                                                                                  math.ceil(len(self.queues[message.server.id]) / 10)))
+                messages.append("{}. {} [{}]".format(idx+1+((page-1)*10), x[0], timedelta(seconds=x[2])))
+            messages.append("{} - {} of {}  -  Page {} of {}  -  Use queue:# for pages".format(10*(page-1), 10*page if 10*page < len(self.queues[message.server.id]) else len(self.queues[message.server.id]),
+                                                                                               len(self.queues[message.server.id]), page,
+                                                                                               math.ceil(len(self.queues[message.server.id]) / 10)))
             return messages
         else:
             for idx, x in enumerate(self.queues[message.server.id][:10]):
-                messages.append("{}. {} [{}]".format(idx, x[0], x[2]))
-            messages.append("1 - 10 of {}  -  Page 1 of {}  -  Use queue # for pages".format(len(self.queues[message.server.id]),
+                messages.append("{}. {} [{}]".format(idx+1, x[0], x[2]))
+            messages.append("1 - 10 of {}  -  Page 1 of {}  -  Use queue:# for pages".format(len(self.queues[message.server.id]),
                                                                                          math.ceil(len(self.queues[message.server.id])/10)))
             return messages
+
+if __name__ == "__main__":
+    Sanxian_Instance = Sanxian()
+    Sanxian_Instance.run(bottoken)
